c958fa5ab19d60fe561f1eb91943866ae9917515
masanori03 <kdbla0408@gmail.com>
2026-06-17 18:24:56 -0400

    Bulid file IO system, task sorting and orgnize codes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from groq import Groq
 from todo_manager import Task
+import json
 
 #load .env file
 load_dotenv()
@@ -11,15 +12,60 @@ client = Groq(api_key =api_key_from_vault)
 # main flow
 task_list = []
 
-task1 = Task("Coop resume","Fix resume for Coop application")
-task2 = Task("Assignment","OOP assignment")
-task3 = Task("Laundry", "Do laundry sometimes")
+#load tasks.json
+if os.path.exists("tasks.json"):
+    with open("tasks.json", "r", encoding="utf-8") as f:
+        load_data = json.load(f)
 
-task_list.append(task1)
-task_list.append(task2)
-task_list.append(task3)
+    for data in load_data:
+        task = Task(data["task"], data["desc"])
+        task.priority = data["priority"]
+        task_list.append(task)
+    print("Todo list loaded")
 
-for task in task_list:
-    task.update_level_with_ai(client)
-    task.show_data()
+
+print("--- Todo Manager Tool ---\n")
+
+while True:
+    
+    user_selection = input("1: Add Todo\n2: Show list with level\n3: Finish\nType Number: ")
     print("\n")
+    
+    # Add Todo
+    if user_selection == "1":
+        print("--- Input the info below ---\n")
+        input_task = input("Task title: \n")
+        input_desc = input("Task descritption: \n")
+        new_task = Task(input_task, input_desc) # Make new instance
+        task_list.append(new_task)
+        print("Task added!\n")
+
+    # AI Sort and show Todo
+    elif user_selection == "2":
+        print("--- Todo list with priority level ---\n")
+        for task in task_list:
+            task.update_level_with_ai(client)
+
+        # this automatically calls __lt__ method in Todo class
+        task_list.sort()
+        
+        for task in task_list:
+            task.show_data()
+            print("\n")
+
+    # Finish the program and save it to json file
+    elif user_selection == "3":
+        
+        save_data = []
+        for task in task_list:
+            save_data.append(task.to_dict())
+
+        with open("tasks.json", "w", encoding="utf-8") as f:
+            json.dump(save_data, f, indent=4)
+
+        print("Data saved to json file")
+        break
+    
+    else: # error handling
+        print("Invalid number! Please enter 1, 2 or 3.\n")
+
--- todo_manager.py
+++ todo_manager.py
@@ -9,10 +9,28 @@ class Task:
     def show_data(self):
         print(f"Task title: {self.task}\nTask desc: {self.desc}\n[Priority level: {self.priority}]")
 
+    
+    # add a weight to priority level
+    def _get_weight(task):
+        if task.priority == "High": # Put weight on each tasks
+            return 1
+        elif task.priority == "Medium":
+            return 2
+        else:
+            return 3
+        
+    # Magic method that calls _get_weight and compare the returned value
+    def __lt__(self, other): 
+        return self._get_weight() < other._get_weight()
 
-# Every time this function is called, the task is sent to the AI and its priority level is evaluated.
+
+    # Every time this function is called, the task is sent to the AI and its priority level is evaluated.
     def update_level_with_ai(self, client):
-
+        
+        # finish this func if already priority updated. This helps reducing resource waste
+        if self.priority is not None:
+            return
+        
         response = client.chat.completions.create(
         model='llama-3.1-8b-instant',
         messages=[
@@ -27,3 +45,10 @@ class Task:
         result = response.choices[0].message.content.strip() # .strip() to remove if there's any extra space
         self.priority = result
   
+  
+    def to_dict(self):
+        return{
+            "task": self.task,
+            "desc": self.desc,
+            "priority": self.priority
+        }
